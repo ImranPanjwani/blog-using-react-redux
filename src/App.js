a47599b67bfe5050b8eb15326634ee425577a105
imranpanjwani <imranpanjwani00@gmail.com>
2020-04-04 16:03:02 +0530

    add nav bar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import DashboardPage from './pages/DashboardPage'
 import PostsPage from './pages/PostsPage'
 import SinglePostPage from './pages/SinglePostPage'
+import { Navbar } from './components/Navbar'
 
 const App = () => (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={DashboardPage} />
         <Route exact path="/posts" component={PostsPage} />
